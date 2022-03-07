eb01f49196bd4c294f2430aa27dc9d6c8ea5efec
Huan <huan.zeng1@gmail.com>
2022-03-07 15:10:24 -0500

    added car controller

--- js/script.js
+++ js/script.js
@@ -1,29 +1,96 @@
+//DOM elements
 const gameboard = document.querySelector('#gameboard')
 
-const gridSize = [{ x: 3, y: 10 }]
-console.log(gameboard)
+//global
+let speed = 5000
 
-const box = document.createElement('div')
-box.style.gridRowStart = 1
-box.style.gridColumnStart = 1
-box.style.backgroundColor = 'black'
+//Game board grid size
+const gridSize = { x: 10, y: 2 }
+//starting car pos
+const car = { x: 9, y: 2 }
+let control = car
 
-gameboard.appendChild(box)
-
-let frameTime = 0
-
-let i = box.style.gridRowStart
-// console.log(i)
-// while (box.style.gridRowStart < 10) {
-//   box.style.gridRowStart = i++
-// }
-
-let interval = 1000
-
-let timer = 0
-
-const moveBlockDown = () => {
-  if (box.style.gridRowStart < 10) box.style.gridRowStart = i++
+//functions
+const createCar = (car) => {
+  const carDiv = document.createElement('div')
+  carDiv.setAttribute('id', 'car')
+  carDiv.style.gridRowStart = car.x
+  carDiv.style.gridColumnStart = car.y
+  carDiv.style.backgroundImage = "url('../image/red.png')"
+  gameboard.appendChild(carDiv)
 }
-let move = setInterval(moveBlockDown, 1000)
-// moveBlockDown()
+const createPothole = () => {
+  const randX = Math.floor(Math.random()) + 1
+  const randY = Math.floor(Math.random() * 3) + 1
+
+  const div = document.createElement('div')
+  div.classList.add('pothole')
+  div.style.gridColumnStart = randY
+  div.style.gridRowStart = randX
+
+  gameboard.appendChild(div)
+}
+const createRandomPothole = () => {
+  const createP = setInterval(createPothole, Math.random() * 5 * 5)
+}
+
+const gameStart = (speed) => {
+  createCar(car)
+  createPothole()
+  let i = 1
+
+  const gameSpeed = setInterval(() => {
+    const pothole = document.querySelectorAll('.pothole')
+    createPothole()
+
+    pothole.forEach((hole) => {
+      if (hole.style.gridRowStart < 10) {
+        hole.style.gridRowStart++
+      }
+    })
+  }, 1000)
+}
+
+gameStart()
+const carPos = document.querySelector('#car')
+//controlling the car
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'ArrowUp':
+      if (carPos.style.gridRowStart < 2) {
+        break
+      }
+      carPos.style.gridRowStart--
+      break
+    case 'ArrowDown':
+      if (carPos.style.gridRowStart > 8) {
+        break
+      }
+      carPos.style.gridRowStart++
+      break
+    case 'ArrowLeft':
+      if (carPos.style.gridColumnStart < 2) {
+        break
+      }
+      carPos.style.gridColumnStart--
+      break
+    case 'ArrowRight':
+      if (carPos.style.gridColumnStart > 2) {
+        break
+      }
+      carPos.style.gridColumnStart++
+      break
+  }
+})
+
+// const gameOver = (pothole) => {
+//   const carX = carPos.style.gridRowStart
+//   const carY = carPos.style.gridColumnStart
+
+//   pothole.forEach((hole) => {
+
+//     pothole.forEach(hole => {
+//       if(hole.)
+//     })
+//   })
+// }
